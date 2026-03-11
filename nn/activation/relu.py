afc00f998db3e4814b60984e691817644abc1514
Rubén <74114026+rubzip@users.noreply.github.com>
2026-03-11 14:09:26 +0100

    Implement ReLU activation function and its derivative

---
+++ nn/activation/relu.py
@@ -0,0 +1,8 @@
+from ..base import Activation
+
+class Relu(Activation):
+    def fn(self, x: np.ndarray) -> np.ndarray:
+        return np.maximum(0, x)
+    
+    def fn_derivative(self, x: np.ndarray) -> np.ndarray:
+        return (x > 0).astype(float)
